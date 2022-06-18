1fc6b8350e816a22bce697eb736c2da59e370325
Rob Cole <robcole@gmail.com>
2022-06-18 16:11:26 -0700

    More small adjustments (WIP for Lucky work)

--- script/build-and-upload-docker-image.sh
+++ script/build-and-upload-docker-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# AMD64 taken care of locally for now.
-docker build -t robcole/crystal:1.4.1-hoard-arm64 --build-arg ARCH=arm64 - < Dockerfile.baseimage
-docker push robcole/crystal:1.4.1-hoard-arm64
-docker manifest create robcole/crystal:1.4.1-hoard --amend robcole/crystal:1.4.1-hoard-amd64 --amend robcole/crystal:1.4.1-hoard-arm64
-docker manifest push --purge robcole/crystal:1.4.1-hoard
+echo "Building Image for $ARCH"
+docker build -t robcole/crystal:lucky-$ARCH --build-arg ARCH=$ARCH - < Dockerfile.baseimage
+docker push robcole/crystal:lucky-$ARCH
+docker manifest create robcole/crystal:lucky --amend robcole/crystal:lucky-amd64 --amend robcole/crystal:lucky-arm64
+docker manifest push --purge robcole/crystal:lucky
