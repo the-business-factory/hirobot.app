38d283e917db92421b474218e4047406d7a74b4a
Rob Cole <robcole@gmail.com>
2022-10-08 17:48:24 -0700

    Upload an old 1.5.1 image as well

--- script/upload_crystal_with_hoard.sh
+++ script/upload_crystal_with_hoard.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 ARCH=${ARCH:-arm64}
-CRYSTAL_VERSION=${CRYSTAL:-1.6.0}
+CRYSTAL_VERSION=${CRYSTAL:-1.5.1}
 
 echo "Building Crystal ${CRYSTAL_VERSION} Image w/Hoard for $ARCH"
 
-docker build -t robcole/crystal:1.6.0-lucky-error-$ARCH --build-arg ARCH=$ARCH - < Dockerfile
-docker push robcole/crystal:1.6.0-lucky-error-$ARCH
-docker manifest create robcole/crystal:1.6.0-lucky-error --amend robcole/crystal:1.6.0-lucky-error-amd64 --amend robcole/crystal:1.6.0-lucky-error-arm64
-docker manifest push --purge robcole/crystal:1.6.0-lucky-error
+docker build -t robcole/crystal:1.5.1-hoard-$ARCH --build-arg ARCH=$ARCH - < Dockerfile.hoard
+docker push robcole/crystal:1.5.1-hoard-$ARCH
+docker manifest create robcole/crystal:1.5.1-hoard --amend robcole/crystal:1.5.1-hoard-amd64 --amend robcole/crystal:1.5.1-hoard-arm64
+docker manifest push --purge robcole/crystal:1.5.1-hoard
