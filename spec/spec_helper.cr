0c94d5ffc629b99fd17b0d53aa399a6718baeff4
Rob Cole <robcole@gmail.com>
2023-02-26 10:11:01 -0800

    Update to Lucky 1.0-rc1

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,6 +2,10 @@ ENV["LUCKY_ENV"] = "test"
 ENV["DEV_PORT"] = "5001"
 require "spec"
 require "lucky_flow"
+require "lucky_flow/ext/lucky"
+require "lucky_flow/ext/avram"
+
+require "lucky_flow/ext/authentic"
 require "../src/app"
 require "./support/flows/base_flow"
 require "./support/**"
@@ -12,8 +16,6 @@ require "../db/migrations/**"
 # By default there are scripts for setting up and cleaning the database,
 # configuring LuckyFlow, starting the app server, etc.
 require "./setup/**"
-
-Avram::SpecHelper.use_transactional_specs(AppDatabase)
 
 include Carbon::Expectations
 include Lucky::RequestExpectations
--- src/utils/performance_trace.cr
+++ src/utils/performance_trace.cr
@@ -1,5 +1,5 @@
 class PerformanceTrace
-  def self.trace(span_name : String, &block)
+  def self.trace(span_name : String, &)
     {% if @top_level.has_constant?("OpenTelemetry") %}
       OpenTelemetry.trace.in_span(span_name) { yield }
     {% else %}
